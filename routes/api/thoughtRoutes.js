bc49789ca896d14de6a1ffaf15fe97ba4780d4bc
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-09-12 18:29:35 -0400

    api routes have been added

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction
+} = require('../../controllers/thoughtController');
+
+// api/thoughts GET all users and Create user
+router.route('/').get(getThoughts).post(createThought);
+
+// api/user/:thoughtId Get user by id, Edit and Delete by user id
+router.route('/:userId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought)
+
+    // /api/thought/:thoughtId/reactions/:reactionId Post and Delete a friend by ID
+router.route('/userId/friends/:friendsId')
+    .post(addReaction)
+    .delete(deleteReaction)
+
+module.exports = router; 
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend
+} = require('../../controllers/userController');
+
+// api/user GET all users and Create user
+router.route('/').get(getUsers).post(createUser);
+
+// api/user/:userId Get user by id, Edit and Delete by user id
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser)
+
+    // /api/users/:userId/friends/:friendId Post and Delete a friend by ID
+router.route('/userId/friends/:friendsId')
+    .post(addFriend)
+    .delete(deleteFriend)
+
+module.exports = router; 
